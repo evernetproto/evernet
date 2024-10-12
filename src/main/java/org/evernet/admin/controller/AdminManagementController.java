e20b86f69ac0a819392acb6f1ea111cd201327a0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2024-10-12 13:15:17 +0530

    Added admin management APIs

---
+++ src/main/java/org/evernet/admin/controller/AdminManagementController.java
@@ -0,0 +1,51 @@
+package org.evernet.admin.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.evernet.admin.model.Admin;
+import org.evernet.admin.request.AdminAdditionRequest;
+import org.evernet.admin.request.AdminPasswordChangeRequest;
+import org.evernet.admin.response.AdminPasswordResponse;
+import org.evernet.admin.service.AdminService;
+import org.evernet.core.auth.AuthenticatedAdminController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AdminManagementController extends AuthenticatedAdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/admins/current")
+    public Admin get() {
+        return adminService.get(getAdminIdentifier());
+    }
+
+    @PutMapping("/admins/current/password")
+    public Admin changePassword(@Valid @RequestBody AdminPasswordChangeRequest request) {
+        return adminService.changePassword(getAdminIdentifier(), request);
+    }
+
+    @PostMapping("/admins")
+    public AdminPasswordResponse add(@Valid @RequestBody AdminAdditionRequest request) {
+        return adminService.add(request, getAdmin());
+    }
+
+    @GetMapping("/admins")
+    public Page<Admin> list(Pageable pageable) {
+        return adminService.list(pageable);
+    }
+
+    @DeleteMapping("/admins/{identifier}")
+    public Admin delete(@PathVariable String identifier) {
+        return adminService.delete(identifier);
+    }
+
+    @PutMapping("/admins/{identifier}/password")
+    public AdminPasswordResponse resetPassword(@PathVariable String identifier) {
+        return adminService.resetPassword(identifier);
+    }
+}
--- src/main/java/org/evernet/admin/service/AdminService.java
+++ src/main/java/org/evernet/admin/service/AdminService.java
@@ -3,16 +3,26 @@ package org.evernet.admin.service;
 import lombok.RequiredArgsConstructor;
 import org.evernet.admin.model.Admin;
 import org.evernet.admin.repository.AdminRepository;
+import org.evernet.admin.request.AdminAdditionRequest;
 import org.evernet.admin.request.AdminInitRequest;
+import org.evernet.admin.request.AdminPasswordChangeRequest;
 import org.evernet.admin.request.AdminTokenRequest;
+import org.evernet.admin.response.AdminPasswordResponse;
 import org.evernet.admin.response.AdminTokenResponse;
 import org.evernet.core.auth.AuthenticatedAdmin;
 import org.evernet.core.auth.Jwt;
 import org.evernet.core.exception.AuthenticationException;
+import org.evernet.core.exception.ClientException;
 import org.evernet.core.exception.NotAllowedException;
+import org.evernet.core.exception.NotFoundException;
 import org.evernet.core.util.Password;
+import org.evernet.core.util.Random;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -51,5 +61,62 @@ public class AdminService {
                 .build(), vertex, vertex);
 
         return AdminTokenResponse.builder().token(token).build();
+    }
+
+    public Admin get(String identifier) {
+        return adminRepository.findByIdentifier(identifier)
+                .orElseThrow(() -> new NotFoundException(String.format("Admin %s not found", identifier)));
+    }
+
+    public Admin changePassword(String identifier, AdminPasswordChangeRequest request) {
+        Admin admin = get(identifier);
+        admin.setPassword(Password.hash(request.getPassword()));
+        return adminRepository.save(admin);
+    }
+
+    public AdminPasswordResponse add(AdminAdditionRequest request, AuthenticatedAdmin creator) {
+        if (exists(request.getIdentifier())) {
+            throw new ClientException(String.format("Admin %s already exists", request.getIdentifier()));
+        }
+
+        String password = Random.generateRandomString(16);
+
+        Admin admin = Admin.builder()
+                .identifier(request.getIdentifier())
+                .password(Password.hash(password))
+                .creator(creator.getIdentifier())
+                .build();
+
+        admin = adminRepository.save(admin);
+
+        return AdminPasswordResponse.builder()
+                .admin(admin)
+                .password(password)
+                .build();
+    }
+
+    public Admin delete(String identifier) {
+        Admin admin = get(identifier);
+        adminRepository.delete(admin);
+        return admin;
+    }
+
+    public Page<Admin> list(Pageable pageable) {
+        return adminRepository.findAll(pageable);
+    }
+
+    public AdminPasswordResponse resetPassword(String identifier) {
+        Admin admin = get(identifier);
+        String password = Random.generateRandomString(16);
+        admin.setPassword(Password.hash(password));
+        admin = adminRepository.save(admin);
+        return AdminPasswordResponse.builder()
+                .password(password)
+                .admin(admin)
+                .build();
+    }
+
+    private Boolean exists(String identifier) {
+        return adminRepository.existsByIdentifier(identifier);
     }
 }
