cf1a60986c92796654b4e3d976ff6077ee69a976
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2024-10-16 17:52:29 +0530

    added actor management APIs

--- src/main/java/org/evernet/actor/controller/ActorController.java
+++ src/main/java/org/evernet/actor/controller/ActorController.java
@@ -1,13 +1,14 @@
 package org.evernet.actor.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.evernet.actor.model.Actor;
+import org.evernet.actor.request.ActorPasswordChangeRequest;
+import org.evernet.actor.request.ActorUpdateRequest;
 import org.evernet.actor.service.ActorService;
 import org.evernet.core.auth.AuthenticatedActorController;
 import org.evernet.core.exception.NotAllowedException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,5 +24,32 @@ public class ActorController extends AuthenticatedActorController {
         }
 
         return actorService.get(getActorIdentifier(), getTargetNodeIdentifier());
+    }
+
+    @PutMapping("/actors/current")
+    public Actor update(@Valid @RequestBody ActorUpdateRequest request) {
+        if (!isLocalActor()) {
+            throw new NotAllowedException();
+        }
+
+        return actorService.update(getActorIdentifier(), request, getTargetNodeIdentifier());
+    }
+
+    @PutMapping("/actors/current/password")
+    public Actor changePassword(@Valid @RequestBody ActorPasswordChangeRequest request) {
+        if (!isLocalActor()) {
+            throw new NotAllowedException();
+        }
+
+        return actorService.changePassword(getActorIdentifier(), request, getTargetNodeIdentifier());
+    }
+
+    @DeleteMapping("/actors/current")
+    public Actor delete() {
+        if (!isLocalActor()) {
+            throw new NotAllowedException();
+        }
+
+        return actorService.delete(getActorIdentifier(), getTargetNodeIdentifier());
     }
 }
