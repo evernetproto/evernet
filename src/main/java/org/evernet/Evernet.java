dabfe743c751fa4950ac149177a8154aaa13384d
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-03-02 17:02:57 +0530

    added evernet server

---
+++ src/main/java/org/evernet/Evernet.java
@@ -0,0 +1,11 @@
+package org.evernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evernet {
+    public static void main(String[] args) {
+        SpringApplication.run(Evernet.class, args);
+    }
+}
