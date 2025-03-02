fa6dee04d7c3bf7b1536d59629844fc829e5f367
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-03-02 22:53:37 +0530

    add node admin APIs

--- src/main/java/org/evernet/node/controller/NodeController.java
+++ src/main/java/org/evernet/node/controller/NodeController.java
@@ -5,11 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.evernet.common.auth.AuthenticatedAdminController;
 import org.evernet.node.model.Node;
 import org.evernet.node.request.NodeCreationRequest;
+import org.evernet.node.request.NodeUpdateRequest;
 import org.evernet.node.service.NodeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,5 +19,20 @@ public class NodeController extends AuthenticatedAdminController {
     @PostMapping("/nodes")
     public Node create(@Valid @RequestBody NodeCreationRequest request) throws Exception {
         return nodeService.create(request, getAdminIdentifier());
+    }
+
+    @PutMapping("/nodes/{identifier}")
+    public Node update(@PathVariable String identifier, @Valid @RequestBody NodeUpdateRequest request) {
+        return nodeService.update(identifier, request);
+    }
+
+    @PutMapping("/nodes/{identifier}/signing-keys")
+    public Node resetSigningKeyPair(@PathVariable String identifier) throws Exception {
+        return nodeService.resetSigningKeyPair(identifier);
+    }
+
+    @DeleteMapping("/nodes/{identifier}")
+    public Node delete(@PathVariable String identifier) {
+        return nodeService.delete(identifier);
     }
 }
--- src/main/java/org/evernet/node/service/NodeService.java
+++ src/main/java/org/evernet/node/service/NodeService.java
@@ -7,8 +7,10 @@ import org.evernet.common.util.Ed25519;
 import org.evernet.node.model.Node;
 import org.evernet.node.repository.NodeRepository;
 import org.evernet.node.request.NodeCreationRequest;
+import org.evernet.node.request.NodeUpdateRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.security.KeyPair;
 import java.util.List;
@@ -47,6 +49,36 @@ public class NodeService {
     public Node get(String identifier) {
         return nodeRepository.findByIdentifier(identifier)
                 .orElseThrow(() -> new NotFoundException(String.format("Node %s not found", identifier)));
+    }
+
+    public Node update(String identifier, NodeUpdateRequest request) {
+        Node node = get(identifier);
+
+        if (StringUtils.hasText(request.getDisplayName())) {
+            node.setDisplayName(request.getDisplayName());
+        }
+
+        node.setDescription(request.getDescription());
+        return nodeRepository.save(node);
+    }
+
+    public Node resetSigningKeyPair(String identifier) throws Exception {
+        Node node = get(identifier);
+
+        KeyPair signingKeyPair = Ed25519.generateKeyPair();
+
+        String signingPrivateKeyString = Ed25519.privateKeyToString(signingKeyPair.getPrivate());
+        String signingPublicKeyString = Ed25519.publicKeyToString(signingKeyPair.getPublic());
+
+        node.setSigningPrivateKey(signingPrivateKeyString);
+        node.setSigningPublicKey(signingPublicKeyString);
+        return nodeRepository.save(node);
+    }
+
+    public Node delete(String identifier) {
+        Node node = get(identifier);
+        nodeRepository.delete(node);
+        return node;
     }
 
     private Boolean identifierExists(String identifier) {
