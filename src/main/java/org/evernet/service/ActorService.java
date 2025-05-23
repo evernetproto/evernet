16c049f446ecff9e5b75bd6f5b84cfee4bdf2789
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-05-23 23:04:46 +0530

    added actor management APIs

--- src/main/java/org/evernet/service/ActorService.java
+++ src/main/java/org/evernet/service/ActorService.java
@@ -9,12 +9,15 @@ import org.evernet.exception.NotFoundException;
 import org.evernet.model.Actor;
 import org.evernet.model.Node;
 import org.evernet.repository.ActorRepository;
+import org.evernet.request.ActorPasswordChangeRequest;
 import org.evernet.request.ActorSignUpRequest;
 import org.evernet.request.ActorTokenRequest;
+import org.evernet.request.ActorUpdateRequest;
 import org.evernet.response.ActorTokenResponse;
 import org.evernet.util.Ed25519KeyHelper;
 import org.evernet.util.Password;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.security.GeneralSecurityException;
 
@@ -73,6 +76,34 @@ public class ActorService {
             throw new NotFoundException(String.format("Actor %s not found on node %s", identifier, nodeIdentifier));
         }
 
+        return actor;
+    }
+
+    public Actor update(String identifier, ActorUpdateRequest request, String nodeIdentifier) {
+        Actor actor = get(identifier, nodeIdentifier);
+
+        if (StringUtils.hasText(request.getDisplayName())) {
+            actor.setDisplayName(request.getDisplayName());
+        }
+
+        actor.setDescription(request.getDescription());
+
+        if (StringUtils.hasText(request.getType())) {
+            actor.setType(request.getType());
+        }
+
+        return actorRepository.save(actor);
+    }
+
+    public Actor changePassword(String identifier, ActorPasswordChangeRequest request, String nodeIdentifier) {
+        Actor actor = get(identifier, nodeIdentifier);
+        actor.setPassword(Password.hash(request.getPassword()));
+        return actorRepository.save(actor);
+    }
+
+    public Actor delete(String identifier, String nodeIdentifier) {
+        Actor actor = get(identifier, nodeIdentifier);
+        actorRepository.delete(actor);
         return actor;
     }
 }
