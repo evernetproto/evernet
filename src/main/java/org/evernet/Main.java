ba5162c98f27092d9e29a9acf7f4d0a3f599df5f
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-06-11 23:32:16 +0530

    added server

--- src/main/java/org/evernet/Main.java
+++ src/main/java/org/evernet/Main.java
@@ -1,7 +1,17 @@
 package org.evernet;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@EnableWebSocket
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
